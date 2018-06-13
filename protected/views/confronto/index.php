ff35439e7d2051595f6b406e4b0b4190660e1178
Diego Oliveira Serra <dieos2@gmail.com>
2018-06-13 20:22:05 -0300

    menos uma

--- protected/views/confronto/index.php
+++ protected/views/confronto/index.php
@@ -150,7 +150,7 @@ var x = setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hours = Math.floor(((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))-1);
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
