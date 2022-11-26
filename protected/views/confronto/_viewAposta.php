cc2761d99df8a05934970c0ca7a4348a6d61d47a
Diego Serra <dieos2@gmail.com>
2022-11-26 18:42:22 -0300

    Update _viewAposta.php

--- protected/views/confronto/_viewAposta.php
+++ protected/views/confronto/_viewAposta.php
@@ -16,7 +16,7 @@ $CriteriaRank = new CDbCriteria();
                                        <td> 
 									   <div class="<?php echo $casa ?>" style="width: 30%;display: initial;">
                                             <label>
-                                                <?php echo ucfirst(CHtml::encode($data->idUser->username)); ?> +  <?php if($rank)echo ucfirst(CHtml::encode($rank->idPonto->pontos)); ?>?></label>
+                                                <?php echo ucfirst(CHtml::encode($data->idUser->username)); ?> +  <?php if($rank)echo ucfirst(CHtml::encode($rank->idPonto->pontos)); ?></label>
                                         </div>
 										</td>
 										<td>
@@ -32,7 +32,7 @@ $CriteriaRank = new CDbCriteria();
 </td>
 										<td><div class="<?php echo $visitante ?>" style="width: 30%;display: initial;">
                                              <label>
-                                                <?php echo ucfirst(CHtml::encode($data->idUser->username)); ?>
+                                                <?php echo ucfirst(CHtml::encode($data->idUser->username)); +  <?php if($rank)echo ucfirst(CHtml::encode($rank->idPonto->pontos)); ?> ?>
 											</label>
                                               
                                         </div></td></tr>
